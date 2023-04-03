a8392699e9456802b962ff452b5131b5709c2726
kuznetsb <kuznetsbog@gmail.com>
2023-04-03 13:14:08 +0300

    Fixed forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -1,20 +1,21 @@
 from django import forms
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
-        queryset=Task.objects.all(),
+        queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
     deadline_datetime = forms.DateTimeField(
         widget=forms.DateTimeInput(
+            format="%Y-%m-%dT%H:%M",
             attrs={
                 "class": "form-control",
                 "placeholder": "select a date",
-                "type": "datetime"
+                "type": "datetime-local"
             }
         ),
         required=False
