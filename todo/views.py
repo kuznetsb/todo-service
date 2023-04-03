a7372863b74cbc246d6fa00bcc94c9b9bde31b2c
kuznetsb <kuznetsbog@gmail.com>
2023-04-03 12:05:33 +0300

    Created TaskList view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskList(generic.ListView):
+    model = Task
+    ordering = ["is_done", "-datetime"]
+    queryset = Task.objects.prefetch_related("tags")
