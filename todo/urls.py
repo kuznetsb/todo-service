f65c7cf813da73523c65bf2bc96f50ae05834c7d
kuznetsb <kuznetsbog@gmail.com>
2023-04-03 12:22:06 +0300

    Refactored naming of TaskList and urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from todo.views import TaskList
+from todo.views import TaskListView
 
 
 urlpatterns = [
-    path("", TaskList.as_view(), name="index")
+    path("", TaskListView.as_view(), name="task-list")
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task
 
 
-class TaskList(generic.ListView):
+class TaskListView(generic.ListView):
     model = Task
     ordering = ["is_done", "-datetime"]
     queryset = Task.objects.prefetch_related("tags")
