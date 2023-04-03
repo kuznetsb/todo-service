5ab01fe87e3c7808e138871d25df535d03a21ec4
kuznetsb <kuznetsbog@gmail.com>
2023-04-03 11:05:06 +0300

    Created models and made migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(null=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self) -> str:
+        status = "Completed" if self.is_done else "Not completed"
+        return f"Task: {self.content} created: {self.datetime} status: {status}"
