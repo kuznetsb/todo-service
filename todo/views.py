c70008fa0a7655bee70ec5e4148c262f6017de51
kuznetsb <kuznetsbog@gmail.com>
2023-04-03 13:14:58 +0300

    Added task createview and url for it

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task
 
 
@@ -9,3 +10,9 @@ class TaskListView(generic.ListView):
     model = Task
     ordering = ["is_done", "-datetime"]
     queryset = Task.objects.prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
